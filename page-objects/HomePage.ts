d20752feed23323fd88b8ddef0b61d94225a2213
Cristiana Fara <cristiana.fara@scalapay.com@Cristianas-MacBook-Pro.local>
2023-09-18 17:35:40 +0200

    Updated home page object!

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -5,12 +5,14 @@ export class HomePage {
     readonly page: Page
     readonly signInButton: Locator
     readonly searchBox: Locator
+    readonly linkFeedback: Locator
 
     // constructor
     constructor(page: Page) {
         this.page = page
         this. signInButton = page.locator("#signin_button")
         this.searchBox = page.locator("#searchTerm")
+        this.linkFeedback = page.locator('#feedback')
     }
 
     // methods
@@ -24,6 +26,10 @@ export class HomePage {
         await this.signInButton.click()
     }
 
+
+    async clickOnFeedbackLink() {
+        await this.linkFeedback.click()
+    }
     // search for
 
     async searchFor(phrase: string) {
